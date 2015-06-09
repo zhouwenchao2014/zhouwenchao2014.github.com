5a7a48506f8d5326e646b3bddc084a1b3bd918ba
zhouwenchao <953685177@qq.com>
2015-06-09 16:59:33 +0800

    modify scroll

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+var scrollTimes = 1;
 window.onload=function(){
 	var a=document.getElementsByClassName("scrollpointer");
 	a[0].style.backgroundColor="#FFAC0C";
 	debugger
+	window.onmousewheel = scrollFunc;
 }
 function selectScoll(value){
 	getChange(value.id);
@@ -21,6 +23,48 @@ function getChange(value){
 		}else{
 			list[i][0].style.display="none";
 			a[i].style.backgroundColor="#FFDA93";
+		}
+	}
+}
+
+var scrollFunc = function(e) {
+	var direct = 0;
+	e = e || window.event;
+	debugger
+	if (e.wheelDelta) { //判断浏览器IE，谷歌滑轮事件             
+		if (e.wheelDelta > 0) { //当滑轮向上滚动时
+			if (scrollTimes <= 1) {
+				scrollTimes = 1;
+			} else {
+				scrollTimes = scrollTimes - 1;
+			}
+			getChange(scrollTimes);
+		}
+		if (e.wheelDelta < 0) { //当滑轮向下滚动时
+			if (scrollTimes >= 4) {
+				scrollTimes = 4;
+			} else {
+				scrollTimes = scrollTimes + 1;
+			}
+			getChange(scrollTimes);
+
+		}
+	} else if (e.detail) { //Firefox滑轮事件
+		if (e.detail > 0) { //当滑轮向上滚动时
+			if (scrollTimes <= 1) {
+				scrollTimes = 1;
+			} else {
+				scrollTimes = scrollTimes - 1;
+			}
+			getChange(scrollTimes);
+		}
+		if (e.detail < 0) { //当滑轮向下滚动时
+			if (scrollTimes >= 4) {
+				scrollTimes = 4;
+			} else {
+				scrollTimes = scrollTimes + 1;
+			}
+			getChange(scrollTimes);
 		}
 	}
 }
