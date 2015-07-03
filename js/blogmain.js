c2f9679ccd2dc1c2b6edfa333035ecf24de1b0e3
zhouwenchao <953685177@qq.com>
2015-07-03 10:04:34 +0800

    add blogmain

---
+++ js/blogmain.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+	var inBar=document.getElementById("inBar");
+	inBar.style.width='296px';
+	var bar=document.getElementById("loadBg");
+	bar.style.display='none';
+	debugger
+}
