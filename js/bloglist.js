262520ad2b973ef8d7408ec2e906f5ffc664f086
zhouwenchao <953685177@qq.com>
2015-07-04 16:30:45 +0800

    modify

---
+++ js/bloglist.js
@@ -0,0 +1,10 @@
+function select(value){
+	var pagenum=document.getElementsByClassName("link");
+	for(var i=0;i<pagenum.length;i++){
+		pagenum[i].style.backgroundColor="";
+	}
+	value.style.backgroundColor="rgb(215, 209, 209)";
+	// rgb(215, 209, 209)
+
+	debugger
+}
